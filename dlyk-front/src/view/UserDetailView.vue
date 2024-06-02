10377e031b5fdfec1f8c985830c6a5506ffd9396
Brian Zhang <ericzhang64@yahoo.com>
2024-06-02 14:58:53 -0400

    add user function done

--- dlyk-front/src/view/UserDetailView.vue
+++ dlyk-front/src/view/UserDetailView.vue
@@ -1,7 +1,69 @@
 <template>
-  <h1>sdsdsd</h1>
-  {{userDetail.id}}
-  {{userDetail.loginAct}}
+  <el-form ref="loginRefForm" :model="userDetail" label-width="120px">
+    <el-form-item label="ID">
+      <div class="detail">&nbsp;{{userDetail.id}}</div>
+    </el-form-item>
+
+    <el-form-item label="账号">
+      <div class="detail">&nbsp;{{userDetail.loginAct}}</div>
+    </el-form-item>
+
+    <el-form-item label="密码">
+      <div class="detail">&nbsp;******</div>
+    </el-form-item>
+
+    <el-form-item label="姓名">
+      <div class="detail">&nbsp;{{userDetail.name}}</div>
+    </el-form-item>
+
+    <el-form-item label="手机">
+      <div class="detail">&nbsp;{{userDetail.phone}}</div>
+    </el-form-item>
+
+    <el-form-item label="邮箱">
+      <div class="detail">&nbsp;{{userDetail.email}}</div>
+    </el-form-item>
+
+    <el-form-item label="账号未过期">
+      <div class="detail">&nbsp;{{userDetail.accountNoExpired == 1 ? "是" : "否"}}</div>
+    </el-form-item>
+
+    <el-form-item label="密码未过期">
+      <div class="detail">&nbsp;{{userDetail.credentialsNoExpired == 1 ? "是" : "否"}}</div>
+    </el-form-item>
+
+    <el-form-item label="账号未锁定">
+      <div class="detail">&nbsp;{{userDetail.accountNoLocked == 1 ? "是" : "否"}}</div>
+    </el-form-item>
+
+    <el-form-item label="账号是否启用">
+      <div class="detail">&nbsp;{{userDetail.accountEnabled == 1 ? "是" : "否"}}</div>
+    </el-form-item>
+
+    <el-form-item label="创建时间">
+      <div class="detail">&nbsp;{{userDetail.createTime}}</div>
+    </el-form-item>
+
+    <el-form-item label="创建人">
+      <div class="detail">&nbsp;{{userDetail.createBy}}</div>
+    </el-form-item>
+
+    <el-form-item label="编辑时间">
+      <div class="detail">&nbsp;{{userDetail.editTime}}</div>
+    </el-form-item>
+
+    <el-form-item label="编辑人">
+      <div class="detail">&nbsp;{{userDetail.editBy}}</div>
+    </el-form-item>
+
+    <el-form-item label="上次登录时间">
+      <div class="detail">&nbsp;{{userDetail.lastLoginTime}}</div>
+    </el-form-item>
+
+    <el-form-item >
+      <el-button type="success" @click="goBack">返回</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 
@@ -32,6 +94,10 @@ export default {
           this.userDetail = res.data.data
         }
       })
+    },
+
+    goBack(){
+      this.$router.go(-1)
     }
   }
 }
@@ -39,5 +105,9 @@ export default {
 
 
 <style scoped>
-
+.detail{
+  width: 100%;
+  background-color: #F0FFFF;
+  padding-left: 15px;
+}
 </style>
--- dlyk-front/src/view/UserView.vue
+++ dlyk-front/src/view/UserView.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-button type="primary">添加用户</el-button>
+  <el-button type="primary" @click="addUser">添加用户</el-button>
   <el-button type="danger">批量删除 </el-button>
 
   <el-table
@@ -23,9 +23,9 @@
 
     <el-table-column label="操作" show-overflow-tooltip >
       <template #default="scope">
-        <el-button type="primary" @click="view(1)">详情</el-button>
-        <el-button type="success" @click="edit()">编辑</el-button>
-        <el-button type="danger" @click="del()">删除</el-button>
+        <el-button type="primary" @click="view(scope.row.id)">详情</el-button>
+        <el-button type="success" @click="edit(scope.row.id)">编辑</el-button>
+        <el-button type="danger" @click="del(scope.row.id)">删除</el-button>
       </template>
     </el-table-column>/
 
@@ -40,11 +40,73 @@
       @next-click="toPage"
       @current-change="toPage"
   />
+
+  <!--用户对话框-->
+  <el-dialog v-model="userDialogVisible" title="新增用户" width="50%" center draggable>
+
+    <el-form ref="userRefForm" :model="userQuery" label-width="120px" :rules="userRules ">
+      <el-form-item label="账号" prop="loginAct">
+        <el-input v-model="userQuery.loginAct"/>
+      </el-form-item>
+
+      <el-form-item label="密码" prop="loginPwd">
+        <el-input type="password" v-model="userQuery.loginPwd"/>
+      </el-form-item>
+
+      <el-form-item label="姓名" prop="name">
+        <el-input v-model="userQuery.name"/>
+      </el-form-item>
+
+      <el-form-item label="手机" prop="phone">
+        <el-input v-model="userQuery.phone"/>
+      </el-form-item>
+
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="userQuery.email"/>
+      </el-form-item>
+
+      <el-form-item label="账号未过期" prop="accountNoExpired">
+        <el-select v-model="userQuery.accountNoExpired" placeholder="请选择" style="width: 100%">
+          <el-option label="是" value="1"/>
+          <el-option label="否" value="0"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="密码未过期" prop="credentialsNoExpired">
+        <el-select v-model="userQuery.credentialsNoExpired" placeholder="请选择" style="width: 100%">
+          <el-option label="是" value="1"/>
+          <el-option label="否" value="0"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="账号未锁定" prop="accountNoLocked">
+        <el-select v-model="userQuery.accountNoLocked" placeholder="请选择" style="width: 100%">
+          <el-option label="是" value="1"/>
+          <el-option label="否" value="0"/>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="账号是否启用" prop="accountEnabled">
+        <el-select v-model="userQuery.accountEnabled" placeholder="请选择" style="width: 100%">
+          <el-option label="是" value="1"/>
+          <el-option label="否" value="0"/>
+        </el-select>
+      </el-form-item>
+    </el-form>
+
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="this.userDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="userSubmit">添加</el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 
 <script>
-  import {doGet} from "../http/httpRequest.js";
+import {doGet, doPost} from "../http/httpRequest.js";
+import {messageTip} from "../util/message.js";
 
   export default {
     name: "UserView",
@@ -54,7 +116,42 @@
         userList: [{}],
         current: 1,
         pageSize: 0,
-        total: 0
+        total: 0,
+        userDialogVisible: false,
+        userQuery: {},
+        userRules: {
+          loginAct: [
+            {required: true, message: '请输入登录账号', trigger: 'blur'}
+          ],
+          loginPwd: [
+            {required: true, message: '请输入登录密码', trigger: 'blur'},
+            {min: 6, max: 16, message: '登录密码长度为6-16位'}
+          ],
+          name: [
+            {required: true, message: '请输入姓名', trigger: 'blur'},
+            {pattern:'[\u4E00-\u9FFF]', message: '姓名必须为中文', trigger:'blur'}
+          ],
+          phone: [
+            {required: true, message: '请输入手机号', trigger: 'blur'},
+            {pattern:'^(?:\\+?86)?1[3-9]\\d{9}$', message: '手机号或者格式有误', trigger:'blur'}
+          ],
+          email: [
+            {required: true, message: '请输入邮箱', trigger: 'blur'},
+            {pattern:'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$', message: '邮箱格式有误', trigger:'blur'}
+          ],
+          accountNoExpired: [
+            {required: true, message: '请选中账号是否过期', trigger: 'blur'},
+          ],
+          credentialsNoExpired: [
+            {required: true, message: '请选择密码是否过期', trigger: 'blur'},
+          ],
+          accountNoLocked: [
+            {required: true, message: '请选择账号是否锁定', trigger: 'blur'},
+          ],
+          accountEnabled: [
+            {required: true, message: '请选择账号是否启用', trigger: 'blur'},
+          ]
+        }
       }
     },
 
@@ -86,6 +183,32 @@
       view(id){
         //跳转到/dashboard/user/1
         this.$router.push('/dashboard/user/' + id)
+      },
+
+      //新增用户
+      addUser(){
+        this.userDialogVisible = true;
+      },
+
+      //新增用户提交
+      userSubmit(){
+        let formData = new FormData();
+        for(let field in this.userQuery){
+          formData.append(field, this.userQuery[field]);
+        }
+
+        this.$refs.userRefForm.validate(valid => {
+          if(valid){
+            doPost('/api/user', formData).then(res=>{
+              if(res.data.code === 200){
+                messageTip('添加成功', 'success')
+              }else {
+                messageTip('添加失败', 'error')
+              }
+            })
+          }
+        })
+
       }
     }
   }
